1558ab48bb4a13dbbe8d2405ec642a4118044bcb
Perttu Autti <65128229+Pertsaa@users.noreply.github.com>
2023-08-21 21:50:30 +0300

    feat: getURLsFromHTML function

--- crawl.js
+++ crawl.js
@@ -9,7 +9,20 @@ function normalizeURL(url) {
   return normalized;
 }
 
-function getURLsFromHTML(htmlBody, baseURL) {}
+function getURLsFromHTML(htmlBody, baseURL) {
+  const dom = new JSDOM(htmlBody);
+  const anchors = dom.window.document.querySelectorAll("a");
+  const hrefs = [];
+  for (const a of anchors) {
+    console.log(a.href, a.href[0] === "/");
+    if (a.href[0] === "/") {
+      hrefs.push(`${baseURL}${a.href}`);
+    } else {
+      hrefs.push(a.href);
+    }
+  }
+  return hrefs;
+}
 
 module.exports = {
   normalizeURL,
